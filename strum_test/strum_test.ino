61a40d10680ea756b6f178d7e45ac8f5a94dd6d4
mattsonsam <mattson.s@northeastern.edu>
2022-02-01 15:27:16 -0500

    Update strum_test.ino

--- strum_test/strum_test.ino
+++ strum_test/strum_test.ino
@@ -12,8 +12,9 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 119;    // variable to store the servo position
-int strum_length=6;
+int pos = 117;    // variable to store the servo position
+int strum_angle=8;
+int deg_per_sec=300; //degrees per secong of MG995 servo
 
 void setup() {
   myservo.attach(4);  // attaches the servo on pin 9 to the servo object
@@ -21,11 +22,11 @@ void setup() {
 
 void loop() {
   /*myservo.write(pos);
-  delay(500);*/
-  myservo.write(pos+strum_length);
-  //delay(500);
+  delay(200);*/
+  myservo.write(pos+strum_angle);
+  delay(250);
   /*myservo.write(pos);
-  delay(500);*/
-  //myservo.write(pos-strum_length);
-  //delay(500);
+  delay(200);*/
+  myservo.write(pos-strum_angle);
+  delay(250);
 }
